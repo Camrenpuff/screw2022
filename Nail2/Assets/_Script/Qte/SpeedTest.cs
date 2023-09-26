fa926a836a6e9f8c4facdf7f9056d6bb3e537856
Camrenpuff <meilun.zhang19@gmail.com>
2023-09-26 11:32:19 +0800

    9-25
    Speed Test

--- Nail2/Assets/_Script/Qte/SpeedTest.cs
+++ Nail2/Assets/_Script/Qte/SpeedTest.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class SpeedTest : MonoBehaviour
 {
     private float speed;
-    public String testTarget;
+    
+    
     public Image circle;
     //public GameObject qtCircle;//图片直接用!!!!
 
     public float fillA = 0;
     private bool grabbing = false;
+    public float timeThreshold = 0;
+    public Rigidbody rigidbody;
 
 
     //void Awake()
@@ -28,45 +31,58 @@ public class SpeedTest : MonoBehaviour
         
         //Debug.Log(qtCircle);
         print(circle.fillAmount);
+         
 
-
-        StartCoroutine(CalcSpeed());
+        //StartCoroutine(CalcSpeed());
     }
 
-    IEnumerator CalcSpeed()
-    {
-        bool isPlaying = true;
-        while (isPlaying)
-        {
-            Vector3 prevPovs = transform.position;
-            yield return new WaitForFixedUpdate();
-            //找到更新的子对象
-            speed = Mathf.RoundToInt(Vector3.Distance(this.transform.Find(testTarget).position, prevPovs) / Time.fixedDeltaTime);
-            print(speed);
+    //IEnumerator CalcSpeed()
+    //{
+    //    bool isPlaying = true;
+    //    while (isPlaying)
+    //    {
+    //        Vector3 prevPovs = transform.position;
+    //        yield return new WaitForFixedUpdate();
+    //        //找到更新的子对象
+    //        //speed = Mathf.RoundToInt(Vector3.Distance(this.transform.Find(testTarget).position, prevPovs) / Time.fixedDeltaTime);
+    //        //speed=Mathf.RoundToInt(Vector3.Distance(screw.transform.position, prevPovs)/Time.fixedDeltaTime);
+            
+    //        print(speed);
            
+            
 
-        }
+    //    }
 
-    }
+    //}
 
     //向QT_Event发消息
     void Update()
     {
-       
 
+        speed = Mathf.Abs(rigidbody.velocity.y);
+        print(speed);
+        print(rigidbody.velocity.y);
         //if (qt_circle != null)
         //{
         //    qt_circle.fillAmount = fillA;
         //}
 
-        if (speed > 0.01 && grabbing == true)
+        if (speed > 1 && grabbing == true)
         {
-            
+           
             print("快速");
-            fillA += 0.01f;
+            fillA += 0.005f;
             print("现在的百分比是" + fillA);
         }
+        timeThreshold += Time.deltaTime;
 
+        if (timeThreshold > 0.1f && speed < 1 && grabbing == true)
+        {
+            timeThreshold= 0;
+            fillA -= 0.01f;
+            
+        }
+        
         circle.fillAmount = fillA;
 
     }
@@ -76,6 +92,13 @@ public class SpeedTest : MonoBehaviour
     {
         grabbing = true;
         print("我抓到螺丝头");
+    }
+
+    void TestFun2()
+    {
+        grabbing = false;
+        print("我放下螺丝头");
+        fillA= 0;
     }
 
 
--- Nail2/Assets/_Script/TriggerAnimation.cs
+++ Nail2/Assets/_Script/TriggerAnimation.cs
@@ -5,23 +5,61 @@ using UnityEngine;
 public class TriggerAnimation : MonoBehaviour
 {
     [SerializeField] private Animator myKey;
-    [SerializeField] private string anim="Kowtow";
+    [SerializeField] private Animator myScrew;
+
+    [SerializeField] private Animator myKey1;
+    [SerializeField] private Animator myScrew1;
+
+    [SerializeField] private Animator myKey2;
+    [SerializeField] private Animator myScrew2;
+
+    //[SerializeField] private Animator myKey3;
+    //[SerializeField] private Animator myScrew3;
+
+    //[SerializeField] private Animator myKey4;
+    //[SerializeField] private Animator myScrew4;
+
+    //[SerializeField] private Animator myKey5;
+    //[SerializeField] private Animator myScrew5;
+
+    //[SerializeField] private Animator myKey6;
+    //[SerializeField] private Animator myScrew6;
+
+    //[SerializeField] private Animator myKey7;
+    //[SerializeField] private Animator myScrew7;
+
+    //[SerializeField] private Animator myKey8;
+    //[SerializeField] private Animator myScrew8;
+
+    //[SerializeField] private Animator myKey9;
+    //[SerializeField] private Animator myScrew9;
+
+    //[SerializeField] private Animator myKey10;
+    //[SerializeField] private Animator myScrew10;
+
+    [SerializeField] private string animKey="Kowtow";
+    [SerializeField] private string animScrew = "Kowtow";
+    [SerializeField] private float offsetTime=1f;
+    [SerializeField] private float offsetTime1 = 1f;
+    [SerializeField] private float offsetTime2 = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            myKey.Play(anim, 0, 0.0f);
-        }
-    }
-    void Start()
-    {
-        
-    }
+            offsetTime = Random.Range(1f, 5f);
+            offsetTime1 = Random.Range(1f, 5f);
 
-  
-    void Update()
-    {
-        
+            myKey.Play(animKey, 0, offsetTime);
+            myScrew.Play(animScrew, 0, offsetTime);
+
+            myKey1.Play(animKey, 0, offsetTime1);
+            myScrew1.Play(animScrew, 0, offsetTime1);
+
+            myKey2.Play(animKey, 0, offsetTime2);
+            myScrew2.Play(animScrew, 0, offsetTime2);
+        }
+
     }
+  
 }
